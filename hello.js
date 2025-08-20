a8f34216aba0ad5d94d79cd406b65c2a75fc02e0
Hussain0349 <=>
2025-08-20 20:22:03 +0500

    Task-1: hello.js also Node.js script

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log('Hello Node.js!')
+
+// print the current data and time 
+
+let time = new Date()
+console.log(`Current Date and Time: ${time}`)
+
+// print the current node js version
+
+console.log(`Node Version:  ${process.version}`)
